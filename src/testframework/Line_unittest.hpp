16862d41d979b5d363638a6ea243691bc8f88df1
Tommy Steimel <trs3ww@virginia.edu>
2014-12-09 22:52:13 -0500

    Wrote header file for line unit test

---
+++ src/testframework/Line_unittest.hpp
@@ -0,0 +1,24 @@
+#ifndef LINE_UNITTEST.H
+#define LINE_UNITTEST.H
+
+#include <iostream>
+#include <cmath>
+#include "../setoflines/Line.hpp"
+
+using namespace std; 
+
+class Line_unittest
+{
+public:
+        int main();
+        bool run_all_tests();
+
+private:
+        bool creation_test();
+        bool add_point_test();
+	bool copy_constructor_test();
+	bool getter_setter_test();
+
+};
+
+#endif
